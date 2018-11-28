62a5c691ebc1f1410438751b21669e129a6dba4d
Pepe Miyashiro <pepe.miyashiro@beauthentic.digital>
2018-11-28 12:10:21 -0500

    #8 Add documentation to the code

--- webpack/webpack-config/webpack.common.js
+++ webpack/webpack-config/webpack.common.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const glob = require('glob');
 const webpackMerge = require('webpack-merge');
-
-
 const commonPaths = require("../webpack-utils/common-paths");
 const entryConfig = require("../webpack-utils/common-split-entries").entryConfig || {};
 
+/**
+ * Common Configuration
+ * configuration shared between Development and Production
+ */
+
+// ! these file should be added dynamically, from a configuration file. for vendors and main
 const baseConfig = {
     main: './src/main.js',
     'web-components.polyfills': './src/libs/web-components.polyfills.js'
--- webpack/webpack-config/webpack.production.js
+++ webpack/webpack-config/webpack.production.js
@@ -1,3 +1,7 @@
+/**
+ * Production Configuration
+ * configuration need in the production build
+ */
 module.exports = {
 
 }
